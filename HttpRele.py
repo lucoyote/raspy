9619bc9992805d6a392e1701b33e6310b633c39f
lucoyote <lucmonti@gmail.com>
2018-03-15 01:00:29 +0100

    Aggiunta documentazione

--- HttpRele.py
+++ HttpRele.py
@@ -3,6 +3,18 @@ import requests
 from timer import IntervalTimer
 
 class HttpRele():
+    """
+    HTTP commanded rele
+
+    Accept command with the follwing syntax:
+    http://{$CommandUrl}?OnOff=$State,TimerOnOff=$EnableTimer,TON=$TimerValue
+    
+    where 
+        $State is the rele command, 0 or 1
+        $EnableTimer is the enabling of the automatic power off
+        $TimerValue is the delay between on and off
+
+    """
     def __init__(self, Name, StateUrl, CommandUrl):
         self.Name = Name
         self.CommandUrl = CommandUrl
@@ -17,6 +29,11 @@ class HttpRele():
         self.State = 0
 
     def Enable(self, EnableState):
+        """
+        Enable HTTP commands
+        Every 1 second pulls the state of the rele, if something change in parameters, send one command packet
+        and refresh state and values
+        """
         if EnableState:
             if not self.__PollingThread.isAlive():
                  self.__PollingThread.start()
@@ -24,6 +41,9 @@ class HttpRele():
             self.__PollingThread.stop()
 
     def PollingFunction (self):
+        """
+        Polling functon, only internal use
+        """
         if (self.OnOff != self.__Requested_OnOff or
             self.TimerOnOff != self.__Requested_TimerOnOff or
             self.TON != self.__Requested_TON):
